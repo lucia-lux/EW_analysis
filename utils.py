eb3193e9f35f051d906761e2f8a819b10313a99d
lucia-lux <luzia.troebinger.11@ucl.ac.uk>
2021-12-27 17:35:50 +0000

    now saving results and figures

---
+++ utils.py
@@ -0,0 +1,154 @@
+   
+import matplotlib.pyplot as plt
+import scikit_posthocs as sp
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from scipy import stats
+from scipy import stats
+
+def kruskal_dunns_func(in_df, time_col, group_name, time_point,outcome):
+    """
+    Check for significant differences using
+    Kruskal Wallis test and Dunn's post-hoc
+
+    Paramters
+    ---------
+    in_df:  pandas Dataframe
+        Input dataframe
+    time_col:   str
+        Name of time point column in in_df
+    group_name: str
+        Groups to compare
+    time_point: int
+        time point for which to compare data
+    outcome:    str
+        outcome to compare on
+    """
+    data = []
+    for group in group_name:
+        data.append(in_df.loc[(in_df.Group==group)&(in_df[time_col]==time_point),outcome].dropna().values)
+    print('Outcome:{},\nstats: {}\n'.format(outcome, stats.kruskal(data[0],data[1],data[2])))
+    print('Posthoc:\n1 = {}, 2 = {}, 3 = {}\n {}\n'.format(group_name[0],group_name[1],
+            group_name[2], sp.posthoc_dunn(data, p_adjust = 'bonferroni')))
+
+    
+def model_checks(data_df, columns_to_use, y_to_use,GEE_res):
+    """
+    Model diagnostics
+
+    Parameters
+    ----------
+    data_df:    pandas DataFrame
+        input dataframe
+    columns_to_use: str
+        names of columns containing predictors to be used in the analysis
+    y_to_use:   str
+        names of outcome column
+    GEE_res:    
+        GEE model results
+
+    -------
+    """
+    gee_res_df = data_df.loc[:,columns_to_use]
+    drop_inds = gee_res_df.loc[gee_res_df[y_to_use].isna(),:].index.values
+    gee_res_df = gee_res_df.drop(labels = drop_inds, axis = 0)
+    gee_res_df['resid_dev'] = GEE_res.resid_deviance
+    gee_res_df['fitted'] = GEE_res.fittedvalues
+    a = GEE_res.resid_deviance
+    a.sort()
+    fig = plt.figure()
+    res = stats.probplot(a,dist = stats.halfnorm,sparams = (-0.18,10), plot=plt)
+    plt.show()
+    # plotting fitted agains residuals
+    g = sns.lmplot(x = "fitted", y = "resid_dev", hue = "Group", data = gee_res_df)
+    g = (g.set_axis_labels("Predicted score", "Deviance residuals"))#.set(xlim=(42, 55)))
+    g = sns.lmplot(x = "time", y = "resid_dev", hue = "Group", data = gee_res_df)
+    g = (g.set_axis_labels("Time [Weeks]", "Deviance residuals").set(xlim=(-1, 14)))
+
+
+def get_qic_table(data_df, formula, group, family_name):
+    """ 
+    Compare models with different covariance structures, based on QIC.
+
+    Parameters
+    ----------
+    data_df:    pandas DataFrame
+        input dataframe
+    formula:    str
+        statsmodels GEE formula (patsy)
+    group:  str
+        column to group by
+    family_name:    str
+        mean response structure name
+    
+    Returns
+    -------
+        Pandas DataFrame containing qic values for each model.
+    """
+    model_covi = smf.gee(formula,group, data_df, cov_struct = sm.cov_struct.Independence(), family = family_name,missing = 'drop').fit()
+    model_covx = smf.gee(formula,group, data_df, cov_struct = sm.cov_struct.Exchangeable(), family = family_name,missing = 'drop').fit()
+    model_covar = smf.gee(formula,group, data_df, cov_struct = sm.cov_struct.Autoregressive(), family = family_name,missing = 'drop').fit()
+    out_df = pd.DataFrame({'dependency type': ['Independence','Exchangeable','Autoregressive'], 'QIC': [model_covi.qic()[0], model_covx.qic()[0],model_covar.qic()[0]], 'QICu': [model_covi.qic()[1], model_covx.qic()[1],model_covar.qic()[1]]})
+    return out_df
+
+
+def run_gee(in_df,group_name,formula,cov_structure, resp_family):
+    """ 
+    Run statsmodels GEE.
+
+    Parameters
+    ----------
+    in_df:  pandas DataFrame
+        input dataframe
+    group_name: str
+        column to group by
+    formula:    str
+        patsy formula
+    cov_structure:  sm covariance structure
+        covariance structure (e.g. sm.cov_struct.Independence())
+    resp_family:    sm family (e.g. sm.families.Tweedie())
+        mean response structure distribution
+
+    Returns
+    -------
+
+    """
+    model = smf.gee(formula,group_name, in_df, cov_struct = cov_structure, family = resp_family,missing = 'drop').fit()
+    qic_df = get_qic_table(in_df,formula,group_name,resp_family)
+    
+    return model, qic_df
+
+
+def model_checks(data_df, columns_to_use, y_to_use,GEE_res):
+    """
+    Model diagnostics
+
+    Parameters
+    ----------
+    data_df:    pandas DataFrame
+        input dataframe
+    columns_to_use: str
+        names of columns containing predictors to be used in the analysis
+    y_to_use:   str
+        names of outcome column
+    GEE_res:    
+        GEE model results
+
+    -------
+    """
+    gee_res_df = data_df.loc[:,columns_to_use]
+    drop_inds = gee_res_df.loc[gee_res_df[y_to_use].isna(),:].index.values
+    gee_res_df = gee_res_df.drop(labels = drop_inds, axis = 0)
+    gee_res_df['resid_dev'] = GEE_res.resid_deviance
+    gee_res_df['fitted'] = GEE_res.fittedvalues
+    a = GEE_res.resid_deviance
+    a.sort()
+    fig = plt.figure()
+    res = stats.probplot(a,dist = stats.halfnorm,sparams = (-0.18,10), plot=plt)
+    plt.show()
+    # plotting fitted agains residuals
+    g = sns.lmplot(x = "fitted", y = "resid_dev", hue = "Group", data = gee_res_df)
+    g = (g.set_axis_labels("Predicted score", "Deviance residuals"))#.set(xlim=(42, 55)))
+    g = sns.lmplot(x = "time", y = "resid_dev", hue = "Group", data = gee_res_df)
+    g = (g.set_axis_labels("Time [Weeks]", "Deviance residuals").set(xlim=(-1, 14)))
